42f4e88f088abfd9c6bc65a8fd7c1a7d64f3b1a4
Kent Fredric <kentfredric@gmail.com>
2014-08-15 14:21:50 +1200

    Build results of 0622ed9 (on master)

--- lib/Dist/Zilla/Plugin/Prereqs/Soften.pm
+++ lib/Dist/Zilla/Plugin/Prereqs/Soften.pm
@@ -4,7 +4,9 @@ use warnings;
 use utf8;
 
 package Dist::Zilla::Plugin::Prereqs::Soften;
-$Dist::Zilla::Plugin::Prereqs::Soften::VERSION = '0.004000';
+
+our $VERSION = '0.004001';
+
 # ABSTRACT: Downgrade listed dependencies to recommendations if present.
 
 our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
@@ -270,7 +272,7 @@ Dist::Zilla::Plugin::Prereqs::Soften - Downgrade listed dependencies to recommen
 
 =head1 VERSION
 
-version 0.004000
+version 0.004001
 
 =head1 SYNOPSIS
 
