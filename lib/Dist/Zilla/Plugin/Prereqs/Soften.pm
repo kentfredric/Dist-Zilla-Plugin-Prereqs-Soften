c6a509802109462449f0909798b18eb4578ff88d
Kent Fredric <kentfredric@gmail.com>
2015-10-03 23:09:01 +1300

    Build results of 53fd71d (on master)

--- lib/Dist/Zilla/Plugin/Prereqs/Soften.pm
+++ lib/Dist/Zilla/Plugin/Prereqs/Soften.pm
@@ -141,8 +141,9 @@ sub _build__copy_to_extras {
   my $self = shift;
   my $to   = [];
   for my $copy ( @{ $self->copy_to } ) {
-    next unless ( my ( $copy_phase, $copy_rel ) = $copy =~ /\A([^.]+)[.](.+)\z/msx );
-    push @{$to}, { phase => $copy_phase, relation => $copy_rel };
+    if ( my ( $copy_phase, $copy_rel ) = $copy =~ /\A([^.]+)[.](.+)\z/msx ) {
+      push @{$to}, { phase => $copy_phase, relation => $copy_rel };
+    }
   }
   return $to;
 }
